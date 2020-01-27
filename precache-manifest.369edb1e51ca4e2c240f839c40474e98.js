d2c8d7c40f4cb8bec74c4033a280998007d9326e
cordwalsh <51129329+cordwalsh@users.noreply.github.com>
2020-01-27 14:04:23 -0800

    Updates

---
+++ precache-manifest.369edb1e51ca4e2c240f839c40474e98.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685ac06a34250b41f6324a57cf7446ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "141f1e95efe96f8a36d5",
+    "url": "/static/css/main.9dc1a3e3.chunk.css"
+  },
+  {
+    "revision": "0b1c25ed1e0a7f626ca9",
+    "url": "/static/js/2.eed27fa5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.eed27fa5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "141f1e95efe96f8a36d5",
+    "url": "/static/js/main.e2393e2b.chunk.js"
+  },
+  {
+    "revision": "92a8fa6d2a61009e0c1c",
+    "url": "/static/js/runtime-main.e3441cc1.js"
+  },
+  {
+    "revision": "a3e35c4e88c1c9e5a7df36cc377abb27",
+    "url": "/static/media/S1_style_1.a3e35c4e.jpg"
+  },
+  {
+    "revision": "1064af2c42a1444e41b41045e4ad5ac0",
+    "url": "/static/media/S1_style_10.1064af2c.jpg"
+  },
+  {
+    "revision": "d1a5ca6518c400079e7c038bd09529b5",
+    "url": "/static/media/S1_style_2.d1a5ca65.jpg"
+  },
+  {
+    "revision": "cff8cbf9f5369fe423d5be5801cd80b7",
+    "url": "/static/media/S1_style_3.cff8cbf9.jpg"
+  },
+  {
+    "revision": "ce2973da90fec4dd1969c796ab1c539a",
+    "url": "/static/media/S1_style_4.ce2973da.jpg"
+  },
+  {
+    "revision": "28357ea1c998619df4b1e25074fc03fc",
+    "url": "/static/media/S1_style_5.28357ea1.jpg"
+  },
+  {
+    "revision": "ed0d208f7d002b0c0c774357df46c791",
+    "url": "/static/media/S1_style_6.ed0d208f.jpeg"
+  },
+  {
+    "revision": "1407c2118403fa0b72d88754c13aa455",
+    "url": "/static/media/S1_style_7.1407c211.png"
+  },
+  {
+    "revision": "737af96d057a7333667d1b2a208b88f4",
+    "url": "/static/media/S1_style_8.737af96d.jpg"
+  },
+  {
+    "revision": "efcadf0ab6605721a5086ac433b2da99",
+    "url": "/static/media/S1_style_9.efcadf0a.jpeg"
+  },
+  {
+    "revision": "cfead8c32ab43b8196171b495d671951",
+    "url": "/static/media/Style_1.cfead8c3.jpg"
+  },
+  {
+    "revision": "fc1a9190bee3bf38dcc0ce5c40ff0254",
+    "url": "/static/media/Style_4.fc1a9190.jpg"
+  },
+  {
+    "revision": "7735f7f49aa8bd4cc941469033ca646c",
+    "url": "/static/media/Style_5.7735f7f4.jpg"
+  },
+  {
+    "revision": "67e21b376c9f29fa5d01022ac4eaec75",
+    "url": "/static/media/Style_7.67e21b37.jpg"
+  },
+  {
+    "revision": "9891350d29afbd1d83a1194f8420cce8",
+    "url": "/static/media/better_doctor.9891350d.gif"
+  },
+  {
+    "revision": "80ee67fd1c96b4aef80469200adbf581",
+    "url": "/static/media/black_14.80ee67fd.jpg"
+  },
+  {
+    "revision": "0850acac552757c76ca5ad4fe42c4d18",
+    "url": "/static/media/black_16.0850acac.jpg"
+  },
+  {
+    "revision": "26a85c61be8fa7b072e90c7369c0ae35",
+    "url": "/static/media/black_20.26a85c61.jpg"
+  },
+  {
+    "revision": "82db331ee03ed3a3346feda0772d4570",
+    "url": "/static/media/black_5.82db331e.jpg"
+  },
+  {
+    "revision": "eb8ecb4a9303b3ec66d6eebcd4dd4423",
+    "url": "/static/media/black_8.eb8ecb4a.jpg"
+  },
+  {
+    "revision": "907701875b702b8377b02e7a4bcfc78e",
+    "url": "/static/media/blue_19.90770187.jpg"
+  },
+  {
+    "revision": "3d0d886e5cccd0fdbba4fbba4f5806e1",
+    "url": "/static/media/blue_26.3d0d886e.jpg"
+  },
+  {
+    "revision": "df011a30bd8adbef26fe41e5f3366c56",
+    "url": "/static/media/blue_4.df011a30.jpg"
+  },
+  {
+    "revision": "c507ddc3361e9c6e8b7ff4d029d87dd9",
+    "url": "/static/media/blue_6.c507ddc3.jpg"
+  },
+  {
+    "revision": "76af6e63648ae2e6b0eec076eda57a5a",
+    "url": "/static/media/blue_8.76af6e63.jpg"
+  },
+  {
+    "revision": "1bff1000e629eb73e0b2f59af2182af5",
+    "url": "/static/media/bw1.1bff1000.jpg"
+  },
+  {
+    "revision": "2a8de5dad916cb7bace5b656f6af09fb",
+    "url": "/static/media/bw3.2a8de5da.jpg"
+  },
+  {
+    "revision": "141a3e1e84a9a199946ffa41ad9463ce",
+    "url": "/static/media/bw4.141a3e1e.jpg"
+  },
+  {
+    "revision": "a02e7815a0755523e513ae00e6968011",
+    "url": "/static/media/bw_mountain.a02e7815.jpg"
+  },
+  {
+    "revision": "186e675554f80ccb488ce91594198e7f",
+    "url": "/static/media/cardigan_15.186e6755.jpg"
+  },
+  {
+    "revision": "3e48ca97fef616a59a8a12999df365d4",
+    "url": "/static/media/cardigan_16.3e48ca97.jpg"
+  },
+  {
+    "revision": "d6a3d28f9e0f6337cfaee92351d883b7",
+    "url": "/static/media/cardigan_21.d6a3d28f.jpg"
+  },
+  {
+    "revision": "320e8819e7ac9b433fc090211cd32565",
+    "url": "/static/media/cardigan_31.320e8819.jpg"
+  },
+  {
+    "revision": "eed30ccbbfd72f0374431fbd834a811c",
+    "url": "/static/media/cardigan_33.eed30ccb.jpg"
+  },
+  {
+    "revision": "3ce896b4e3ad3db87232f584eb2af30d",
+    "url": "/static/media/cardigan_detail.3ce896b4.jpg"
+  },
+  {
+    "revision": "7f525697179def9cf2c577ca1de651fe",
+    "url": "/static/media/cardigan_detail_2.7f525697.jpg"
+  },
+  {
+    "revision": "21342bb02ea2c159ac08df6e1d8260e8",
+    "url": "/static/media/city_bw.21342bb0.jpg"
+  },
+  {
+    "revision": "cc983c03290dc58e729eca1778eff412",
+    "url": "/static/media/epicarcades.cc983c03.gif"
+  },
+  {
+    "revision": "364098908d7cc897019a1dab138e60d9",
+    "url": "/static/media/four_1.36409890.jpg"
+  },
+  {
+    "revision": "5647581b24ba965dcc1d6087c23936dd",
+    "url": "/static/media/four_2.5647581b.jpg"
+  },
+  {
+    "revision": "06ed9fb0c5718668f6a3f46b4554bca6",
+    "url": "/static/media/four_3.06ed9fb0.jpg"
+  },
+  {
+    "revision": "c3580c6adbf10bd584377868c1392266",
+    "url": "/static/media/four_4.c3580c6a.jpg"
+  },
+  {
+    "revision": "d4bf4083749a2b68886121313b1df761",
+    "url": "/static/media/four_5.d4bf4083.jpg"
+  },
+  {
+    "revision": "72228bb027c7159a274847193aef3dda",
+    "url": "/static/media/four_6.72228bb0.jpg"
+  },
+  {
+    "revision": "f73febb75b21ebf73062906c8c6099ba",
+    "url": "/static/media/four_7.f73febb7.jpg"
+  },
+  {
+    "revision": "8fa0ce103e3a130ffea7b48be7d806f8",
+    "url": "/static/media/four_8.8fa0ce10.jpg"
+  },
+  {
+    "revision": "9d4f670e836b56b3bcb391654d074b69",
+    "url": "/static/media/four_9.9d4f670e.jpg"
+  },
+  {
+    "revision": "456ef04a50f7dabe90833acd521e9a15",
+    "url": "/static/media/head.456ef04a.jpeg"
+  },
+  {
+    "revision": "09338ecceeed24a41364e684ad9b9e61",
+    "url": "/static/media/help_queue.09338ecc.gif"
+  },
+  {
+    "revision": "08dd316486fbc8daf83574351536c096",
+    "url": "/static/media/karaoke.08dd3164.gif"
+  },
+  {
+    "revision": "29ad4361ea284488f95e362fdececb12",
+    "url": "/static/media/smoking_jacket.29ad4361.jpg"
+  },
+  {
+    "revision": "c8957e1bd059f8fd7a783ae8fe89daa4",
+    "url": "/static/media/smoking_jacket_standing.c8957e1b.jpeg"
+  },
+  {
+    "revision": "bf23c1fd0de999eed34b1af434c20914",
+    "url": "/static/media/tap_a_keg.bf23c1fd.gif"
+  },
+  {
+    "revision": "4852e176e0b87aa26e0dd2b86f905833",
+    "url": "/static/media/winnie_the_pooh.4852e176.gif"
+  }
+]);
