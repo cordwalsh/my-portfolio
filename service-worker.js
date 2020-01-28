4c03a73eef0fc3b196037e2074bff69a5a63dda5
cordwalsh <51129329+cordwalsh@users.noreply.github.com>
2020-01-27 16:06:51 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.d81d165664977dc4f5afc4178097596a.js"
+  "/my-portfolio/precache-manifest.c75c99b6410da25a073566f63dbdeee4.js"
 );
 
 self.addEventListener('message', (event) => {
